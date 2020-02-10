0fc0d3d04f3da6414ae5636c445d9f1b7e70d33c
Julien Verlaguet <julien.verlaguet@gmail.com>
2020-02-10 07:33:55 +0000

    gave up on the idea of building with clang

--- build_submodules.sh
+++ build_submodules.sh
@@ -10,44 +10,6 @@ mkdir -p $TPDIR
 rm -f Makefile.config
 touch Makefile.config
 
-if [ -z $CLANG ]; then
-    CLANG=`which clang`
-fi
-
-EXPVERSION=5
-
-if [ -f $CLANG ]; then
-    VERSION=`$CLANG --version | grep version | sed 's/[^0-9]*\([0-9]*\).*/\1/'`
-    if [ $((VERSION)) -lt $((EXPVERSION)) ]; then
-	echo "ERROR: clang compiler version is too old!"
-	echo "ERROR: SKIP needs at least version $EXPVERSION"
-	echo "ERROR: your version is $VERSION"
-	exit 2
-    fi
-    echo "clang compiler found: $CLANG"
-else
-    echo "ERROR: clang compiler not found"
-    exit 2
-fi
-
-if [ -z $CLANGXX ]; then
-    CLANGXX=`which clang++`
-fi
-
-if [ -f $CLANGXX ]; then
-    VERSION=`$CLANGXX --version | grep version | sed 's/[^0-9]*\([0-9]*\).*/\1/'`
-    if [ $((VERSION)) -lt $((EXPVERSION)) ]; then
-	echo "ERROR: clang compiler version is too old!"
-	echo "ERROR: SKIP needs at least version $EXPVERSION"
-	echo "ERROR: your version is $VERSION"
-	exit 2
-    fi
-    echo "clang++ compiler found: $CLANGXX"
-else
-    echo "ERROR: clang++ compiler not found"
-    exit 2
-fi
-
 echo "-------------------------------------------------------------------------------"
 echo "- Checking that the files are there -"
 echo "-------------------------------------------------------------------------------"
@@ -63,7 +25,7 @@ echo "- Building JEMALLOC -"
 echo "-------------------------------------------------------------------------------"
 
 (cd ./third-party/jemalloc/src && autoconf)
-(cd ./third-party/jemalloc/src && ./configure CC=$CLANG CXX=$CLANGXX --prefix=$TPDIR --with-mangling)
+(cd ./third-party/jemalloc/src && ./configure --prefix=$TPDIR --with-mangling)
 (cd ./third-party/jemalloc/src && make -j 16 build_lib_static)
 (cd ./third-party/jemalloc/src && make install_bin install_include install_lib)
 cat $TPDIR/include/jemalloc/jemalloc.h | grep -v "define je_posix_memalign posix_memalign" > $TPDIR/include/jemalloc/jemalloc_hack.h
@@ -75,7 +37,7 @@ echo "--------------------------------------------------------------------------
 
 export ICUFLAGS="-DUNISTR_FROM_CHAR_EXPLICIT=explicit -DUNISTR_FROM_STRING_EXPLICIT=explicit -DU_NO_DEFAULT_INCLUDE_UTF_HEADERS=1 -DU_USING_ICU_NAMESPACE=0 -DU_CHARSET_IS_UTF8=1"
 (cd third-party/icu/src/icu4c/source/ && \
-  CC=$CLANG CXX=$CLANGXX CPPFLAGS="$ICUFLAGS" CFLAGS="$ICUFLAGS" ./runConfigureICU Linux --disable-shared --enable-static --disable-tests --disable-samples --prefix="$TPDIR")
+  CPPFLAGS="$ICUFLAGS" CFLAGS="$ICUFLAGS" ./runConfigureICU Linux --disable-shared --enable-static --disable-tests --disable-samples --prefix="$TPDIR")
 (cd third-party/icu/src/icu4c/source/ && make -j 16 && make install)
 (cd third-party/icu/src/icu4c/source/tools/escapesrc/ && rm -f *.o)
 
@@ -100,7 +62,7 @@ cp third-party/pcre/src/m4/libtool.m4 third-party/pcre/src/m4/libtool.m4.back
 cp third-party/pcre/src/missing third-party/pcre/src/missing.back
 cp third-party/pcre/src/test-driver third-party/pcre/src/test-driver.back
 
-(cd third-party/pcre/src/ && autoreconf -f -i && CC=$CLANG CXX=$CLANGXX ./configure --prefix="$TPDIR" --enable-static --disable-shared --enable-utf && make -j 16 && make install)
+(cd third-party/pcre/src/ && autoreconf -f -i && ./configure --prefix="$TPDIR" --enable-static --disable-shared --enable-utf && make -j 16 && make install)
 
 
 cp third-party/pcre/src/INSTALL.back third-party/pcre/src/INSTALL
@@ -124,5 +86,5 @@ if [ "$(uname)" != "Darwin" ]; then
     echo "- Building LIBUNWIND -"
     echo "-------------------------------------------------------------------------------"
 
-    (cd third-party/libunwind && ./autogen.sh && CC=$CLANG CXX=$CLANGXX ./configure --prefix="$TPDIR" --disable-minidebuginfo && make -j 16 && make install)
+    (cd third-party/libunwind && ./autogen.sh && ./configure --prefix="$TPDIR" --disable-minidebuginfo && make -j 16 && make install)
 fi
